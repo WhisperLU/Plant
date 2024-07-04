d4202d3447c66b31e715be73f524888663160ff8
hefeifei <heff@aeroht.com>
2024-07-04 18:37:03 +0800

    camera

--- Plant/menu_scene.h
+++ Plant/menu_scene.h
@@ -3,6 +3,7 @@
 #define _MENU_SCENE_H_
 #include "scene.h"
 #include "atlas.h"
+#include "camera.h"
 #include "animation.h"
 #include "scene_manager.h"
 
@@ -20,22 +21,33 @@ public:
 
 	void on_enter()
 	{
-		std::cout << "进入主菜单" << std::endl;
+		//std::cout << "进入主菜单" << std::endl;
 		animation_peashooter_run_right.set_atlas(&atlas_peashooter_run_right);
 		animation_peashooter_run_right.set_interval(75);
 		animation_peashooter_run_right.set_loop(true);
+
+		//animation_peashooter_run_right.set_callback(
+		//	[]()
+		//	{
+		//		scene_manager.switch_to(SceneManager::SceneType::Game);
+		//	}
+		//);// lambda函数，动画播放结束后切换场景到游戏场景
+
+		// 通用定时器和摄像机特效 02：21
 	}
 
 	void on_update(int delta)
 	{
-		std::cout << "主菜单正在运行" << std::endl;
+		//std::cout << "主菜单正在运行" << std::endl;
+		camera.on_update(delta);
 		animation_peashooter_run_right.on_update(delta);
 	}
 
 	void on_draw()
 	{
-		outtextxy(10, 10, _T("主菜单绘图内容"));
-		animation_peashooter_run_right.on_draw(100, 100);// 游戏摄像机基础实现  06:16
+		//outtextxy(10, 10, _T("主菜单绘图内容"));
+		const Vector2& pos_camera = camera.get_position();
+		animation_peashooter_run_right.on_draw((int)(100 - pos_camera.x), (int)(100 - pos_camera.y));
 	}
 
 	void on_input(const ExMessage& msg)
@@ -52,7 +64,7 @@ public:
 	}
 
 private:
-
+	Camera camera;
 	Animation animation_peashooter_run_right;
 
 
